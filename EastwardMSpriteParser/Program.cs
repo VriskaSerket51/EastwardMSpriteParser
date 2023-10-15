093808e351212f1ec00de05663f1edfe7b5b1de4
vriskaserket51 <serketvriska51@gmail.com>
2023-10-15 22:47:00 +0900

    Initial Commit

--- EastwardMSpriteParser/Program.cs
+++ EastwardMSpriteParser/Program.cs
@@ -1,3 +1,7 @@
 ﻿using System.Drawing;
 using EastwardMSpriteParser;
 
+string json = File.ReadAllText(@"anim\npc_sam.json");
+Image texture = Image.FromFile(@"anim\npc_sam_texture.png");
+var mSprite = new MSprite(json, texture);
+mSprite.ExtractTo(@"anim\frames");

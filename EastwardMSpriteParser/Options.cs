9d83b807c21ee1825673ab8fef294c375cebdedb
vriskaserket51 <serketvriska51@gmail.com>
2023-10-15 22:57:49 +0900

    Initial Commit

--- EastwardMSpriteParser/Options.cs
+++ EastwardMSpriteParser/Options.cs
@@ -12,4 +12,7 @@ public class Options
 
     [Option('o', "output_dir", Required = true, HelpText = "Set output directory.")]
     public string OutputDirectory { get; set; }
+    
+    [Option('s', "size", Required = false, HelpText = "Set size multiplier. Default value is 1", Default = 1)]
+    public int SizeMultiplier { get; set; }
 }
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Eastward MSprite Parser
 
 Read ".msprite" file and texture file, then extract to output directory.
 
-TODO: Make frames to ".gif" file.
+TODO:
+- Make frames to ".gif" file.
+- Size Multiplying.
